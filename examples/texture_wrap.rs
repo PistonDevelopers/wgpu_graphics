19a18f2248258d74018d16cdbf3f8661550bdc2d
Sven Nilsen <bvssvni@gmail.com>
2023-09-26 08:03:55 +0200

    Published 0.9.0
    - Updated dependencies

--- examples/texture_wrap.rs
+++ examples/texture_wrap.rs
@@ -16,8 +16,8 @@ fn main() {
         (640, 480),
     ));
 
-    let instance = wgpu::Instance::new(wgpu::Backends::all());
-    let surface = unsafe { instance.create_surface(window.get_window()) };
+    let instance = wgpu::Instance::new(Default::default());
+    let surface = unsafe { instance.create_surface(window.get_window()) }.unwrap();
     let adapter =
         futures::executor::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
             compatible_surface: Some(&surface),
